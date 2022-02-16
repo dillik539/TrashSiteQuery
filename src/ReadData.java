15d9714537d142817f2f490043c559a975d6c3f3
Dilli Khatiwoda <dilli.khatiwoda@yahoo.com>
2022-02-16 17:38:51 -0600

    This class reads the data from the file and process it.

---
+++ src/ReadData.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+/**
+ * @author dilli
+ *
+ */
+public class ReadData {
+	private String path;
+
+	/**
+	 * 
+	 */
+	public ReadData(String path) {
+		this.setPath(path);
+	}
+
+	/**
+	 * @return the path
+	 */
+	public String getPath() {
+		return path;
+	}
+
+	/**
+	 * @param path the path to set
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String[] read() {
+
+		return null;
+	}
+
+}
